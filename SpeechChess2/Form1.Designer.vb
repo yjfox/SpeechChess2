70e8c5148e49a60c0bd1b49f7867befc19d14739
Jun Yin <yjfoxer@gmail.com>
2013-11-21 11:30:59 -0500

    speech control program - a five chess game
    Gomoku

---
+++ SpeechChess2/Form1.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.txtBox = New System.Windows.Forms.TextBox()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.Cmd_close = New System.Windows.Forms.Button()
+        Me.cmdrestart = New System.Windows.Forms.Button()
+        Me.cmdclose = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Me_Draw = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Me_Draw, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(34, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Chess Status"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(243, 28)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(75, 13)
+        Me.lblStatus.TabIndex = 1
+        Me.lblStatus.Text = "Record Status"
+        '
+        'txtBox
+        '
+        Me.txtBox.Location = New System.Drawing.Point(37, 50)
+        Me.txtBox.Multiline = True
+        Me.txtBox.Name = "txtBox"
+        Me.txtBox.Size = New System.Drawing.Size(291, 86)
+        Me.txtBox.TabIndex = 2
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(37, 150)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(75, 23)
+        Me.btnStart.TabIndex = 4
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.Location = New System.Drawing.Point(254, 148)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(75, 23)
+        Me.btnStop.TabIndex = 5
+        Me.btnStop.Text = "Stop"
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'Cmd_close
+        '
+        Me.Cmd_close.Location = New System.Drawing.Point(147, 148)
+        Me.Cmd_close.Name = "Cmd_close"
+        Me.Cmd_close.Size = New System.Drawing.Size(75, 23)
+        Me.Cmd_close.TabIndex = 6
+        Me.Cmd_close.Text = "Exit"
+        Me.Cmd_close.UseVisualStyleBackColor = True
+        '
+        'cmdrestart
+        '
+        Me.cmdrestart.Location = New System.Drawing.Point(147, 198)
+        Me.cmdrestart.Name = "cmdrestart"
+        Me.cmdrestart.Size = New System.Drawing.Size(75, 23)
+        Me.cmdrestart.TabIndex = 7
+        Me.cmdrestart.Text = "Game Start"
+        Me.cmdrestart.UseVisualStyleBackColor = True
+        '
+        'cmdclose
+        '
+        Me.cmdclose.Location = New System.Drawing.Point(147, 250)
+        Me.cmdclose.Name = "cmdclose"
+        Me.cmdclose.Size = New System.Drawing.Size(75, 23)
+        Me.cmdclose.TabIndex = 8
+        Me.cmdclose.Text = "Restart"
+        Me.cmdclose.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(207, 311)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 130)
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
+        '
+        'Me_Draw
+        '
+        Me.Me_Draw.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Me_Draw.BackgroundImage = CType(resources.GetObject("Me_Draw.BackgroundImage"), System.Drawing.Image)
+        Me.Me_Draw.Location = New System.Drawing.Point(493, 121)
+        Me.Me_Draw.Name = "Me_Draw"
+        Me.Me_Draw.Size = New System.Drawing.Size(340, 368)
+        Me.Me_Draw.TabIndex = 9
+        Me.Me_Draw.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackgroundImage = CType(resources.GetObject("PictureBox2.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(517, 119)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(303, 11)
+        Me.PictureBox2.TabIndex = 11
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackgroundImage = CType(resources.GetObject("PictureBox3.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(492, 132)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(11, 334)
+        Me.PictureBox3.TabIndex = 12
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackgroundImage = CType(resources.GetObject("PictureBox4.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox4.Location = New System.Drawing.Point(508, 141)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(318, 326)
+        Me.PictureBox4.TabIndex = 13
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BackgroundImage = CType(resources.GetObject("PictureBox5.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox5.Location = New System.Drawing.Point(106, 311)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(100, 130)
+        Me.PictureBox5.TabIndex = 14
+        Me.PictureBox5.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(984, 554)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Me_Draw)
+        Me.Controls.Add(Me.cmdclose)
+        Me.Controls.Add(Me.cmdrestart)
+        Me.Controls.Add(Me.Cmd_close)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.txtBox)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Name = "Form1"
+        Me.Text = "Gomoku"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Me_Draw, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblStatus As System.Windows.Forms.Label
+    Friend WithEvents txtBox As System.Windows.Forms.TextBox
+    Friend WithEvents btnStart As System.Windows.Forms.Button
+    Friend WithEvents btnStop As System.Windows.Forms.Button
+    Friend WithEvents Cmd_close As System.Windows.Forms.Button
+    Friend WithEvents cmdrestart As System.Windows.Forms.Button
+    Friend WithEvents cmdclose As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Me_Draw As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox5 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ SpeechChess2/Form1.vb
@@ -0,0 +1,451 @@
+﻿Imports SpeechLib
+'Copyright - developed for Interactive System Homework - speech program
+'Group members - J.Yin, C.Zhou, R.Zhang 
+
+Public Class Form1
+    Dim WithEvents RecoContext As SpSharedRecoContext      'The Main Recognition Object Used throughout the whole program. -- Shared Object: More Info on this later.
+
+    Dim Grammar As ISpeechRecoGrammar                      'The Grammar Object so the program knows what is going on. -- Instanced Object: More Info on this later.
+
+    Dim CharCount As Integer                                'This is used to count the amount of chars that are in the text box.
+
+
+
+
+    Private Sub btnStart_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStart.Click
+
+
+
+        'First check to see if reco has been loaded before. If not lets load it.
+        If (RecoContext Is Nothing) Then
+
+            RecoContext = New SpSharedRecoContext        'Create a new Reco Context Class
+
+            Grammar = RecoContext.CreateGrammar           'Setup the Grammar
+
+            Grammar.CmdLoadFromFile("c:\sol.xml")           'Load the Grammar
+
+        End If
+
+
+
+        lblStatus.Text = "Recognition Started"                'Change the Label to let the user know whats up
+
+        Grammar.CmdSetRuleIdState(0, SpeechRuleState.SGDSActive)   'Turns on the Recognition with my grammar. This is Vitally important
+
+
+
+        'This is so the user doesn't break the program by
+
+        'trying to start the recognition after its already started.
+
+        btnStart.Enabled = False
+
+        btnStop.Enabled = True
+
+    End Sub
+    'Stop Button. This will stop stop the recoginition
+
+    Private Sub btnStop_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStop.Click
+
+
+
+        Grammar.DictationSetState(SpeechRuleState.SGDSInactive) 'Turns off the Recognition. It will go dormant.
+
+        lblStatus.Text = "Recognition Stopped"                'Change the label to let the user know whats up
+
+
+
+        'Again This is so the user doesn't go breaking things accidently
+
+        btnStart.Enabled = True
+
+        btnStop.Enabled = False
+
+    End Sub
+
+
+    'This is the hypothesis sub. The hypothesis is not the final recognition. This will fire many times per word. You do not want to print anything that is final from the hypothesis.
+
+    'This is not required for the final recognition. But it is vital to understand it.
+
+    Private Sub OnHypo(ByVal StreamNumber As Integer, ByVal StreamPosition As Object, ByVal Result As ISpeechRecoResult) Handles RecoContext.Hypothesis
+
+
+
+        btnStop.Enabled = False 'Don't allow the user to stop the recognition until it has completed.
+
+        'The button will re-enable in the OnReco Event
+
+
+        'This is so you don't kepp printing the same text over and over. It could take up just a tiny bit more processor power
+
+        'Its good to not do un-needed things.
+
+        If lblStatus.Text <> "Receiving" Then
+
+            lblStatus.Text = "Receiving"
+
+        End If
+
+    End Sub
+    Private Sub OnReco(ByVal StreamNumber As Integer, ByVal StreamPosition As Object, ByVal RecognitionType As SpeechRecognitionType, ByVal Result As ISpeechRecoResult) Handles RecoContext.Recognition
+
+        Dim recoResult As String = Result.PhraseInfo.GetText 'Create a new string, and assign the recognized text to it.
+
+        'If you would prefer to use the SendKeys method, Comment out this entire block. And Uncomment the SendKeys Line.
+        txtBox.Selectionstart = CharCount
+        If recoResult = "begin" Then
+
+            txtBox.SelectedText = recoResult & Chr(10)
+            CharCount = CharCount + 1 + Len(recoResult)
+            Call cmdrestart_Click()
+            RecoContext.Voice.Speak("now game start,black's turn at first, please put black chess piece down!")
+        End If
+
+        Dim wordX(14) As String
+        wordX(0) = "one"
+        wordX(1) = "two"
+        wordX(2) = "three"
+        wordX(3) = "four"
+        wordX(4) = "five"
+        wordX(5) = "six"
+        wordX(6) = "seven"
+        wordX(7) = "eight"
+        wordX(8) = "nine"
+        wordX(9) = "ten"
+        wordX(10) = "eleven"
+        wordX(11) = "twelve"
+        wordX(12) = "thirteen"
+        wordX(13) = "fourteen"
+        wordX(14) = "fifteen"
+        Dim wordY(14) As String
+        wordY(0) = "one"
+        wordY(1) = "two"
+        wordY(2) = "three"
+        wordY(3) = "four"
+        wordY(4) = "five"
+        wordY(5) = "six"
+        wordY(6) = "seven"
+        wordY(7) = "eight"
+        wordY(8) = "nine"
+        wordY(9) = "ten"
+        wordY(10) = "eleven"
+        wordY(11) = "twelve"
+        wordY(12) = "thirteen"
+        wordY(13) = "fourteen"
+        wordY(14) = "fifteen"
+
+
+        Dim wX As Integer
+        Dim wY As Integer
+
+        'map word x and y to integer x and y
+        For wX = 0 To 14 Step 1
+            For wY = 0 To 14 Step 1
+                If recoResult = wordX(wX) & " " & wordY(wY) Then
+                    txtBox.SelectedText = recoResult & Chr(10)
+                    wX = wX + 1
+                    wY = wY + 1
+                    Me_Draw_VoiceDown(wX, wY)
+                End If
+            Next wY
+        Next wX
+
+
+
+
+        lblStatus.Text = "Finished Dictating"
+
+        btnStop.Enabled = True
+
+    End Sub
+
+
+    '==================================================================================
+    'Speech ends, Five chess begins
+    '==================================================================================
+
+    Dim blackturn As Boolean 'black turn 
+    Dim whiteturn As Boolean 'white turn
+    Dim table(0 To 15, 0 To 15) As Integer 'arrary stores the current status of chess game
+    Dim inti As Integer 'array(intx,inty)
+    Dim intj As Integer
+    Dim intx As Integer 'x coordinate 
+    Dim inty As Integer 'y coordinate
+    Dim boolstatus As Boolean 'indicate chess status : running or end
+    'draw module
+    Dim mybitmap As Bitmap
+    Dim Graph As Graphics
+
+    'judge if there is a winner
+    Private Sub judgeman()
+
+        Dim strwho As String 'potential winner 
+
+        If table(inti, intj) = 1 Then '1=black, otherwise white 
+            strwho = "Black"
+        Else
+            strwho = "White"
+        End If
+
+
+        If samelinenums(1, 0) >= 5 Or samelinenums(0, 1) >= 5 Or samelinenums(1, 1) >= 5 Or samelinenums(-1, 1) >= 5 Then
+            PictureBox5.Image = Image.FromFile("C:\Users\JunY\Documents\Visual Studio 2012\Projects\SpeechChess2\nan_win.png")
+            MsgBox(strwho & " Win!")
+            boolstatus = False 'chess end status flag
+
+        End If
+    End Sub
+
+    'detect how many pieces in one line
+    Function samelinenums(changei As Integer, changej As Integer)
+        Dim i As Integer
+        Dim j As Integer
+        Dim num As Integer 'how many pieces in one line
+
+        'one side
+        i = inti : j = intj
+        Do
+            If table(i, j) <> table(inti, intj) Then
+                num = max(Math.Abs(inti - i), Math.Abs(intj - j))
+                Exit Do
+            End If
+            i = i + changei : j = j + changej
+        Loop Until i < 0 Or i > 15 Or j < 0 Or j > 15
+
+        'the other side
+        i = inti : j = intj
+        Do
+            If table(i, j) <> table(inti, intj) Then
+                num = num - 1 + max(Math.Abs(inti - i), Math.Abs(intj - j))
+                Exit Do
+            End If
+            i = i - changei : j = j - changej
+        Loop Until i < 0 Or i > 15 Or j < 0 Or j > 15
+        'MsgBox num 
+        samelinenums = num
+    End Function
+
+    'biggest number of two 
+    Function max(inta As Integer, intb As Integer)
+        max = inta
+        If max < intb Then max = intb
+    End Function
+
+    'draw a chessborad which is 16*16 and length of 20pix
+    Private Sub Me_Draw_Paint()
+
+        mybitmap = New Bitmap(Me_Draw.Width, Me_Draw.Height)
+        Me_Draw.Image = mybitmap
+        Graph = Graphics.FromImage(Me_Draw.Image)
+
+        Dim i As Integer
+        'AutoScaleMode = 3  
+        For i = 10 To 330 Step 20
+            Graph.DrawLine(Pens.Black, 10, i, 330, i)
+            Graph.DrawLine(Pens.Black, i, 10, i, 330)
+        Next
+
+    End Sub
+
+    Private Sub Me_Draw_PaintCircle(flag As Integer, x As Integer, y As Integer) 'begin at (10,10),draw chessboard which is 16*16,length of 20pix 
+
+        If flag = 1 Then
+            'Call Me_Draw_Paint()
+            Me_Draw.Image = mybitmap
+            Graph = Graphics.FromImage(Me_Draw.Image)
+            Graph.FillEllipse(Brushes.Black, x - 5, y - 5, 10, 10)
+        Else
+            'Call Me_Draw_Paint()
+            Me_Draw.Image = mybitmap
+            Graph = Graphics.FromImage(Me_Draw.Image)
+            Graph.FillEllipse(Brushes.White, x - 5, y - 5, 10, 10)
+        End If
+    End Sub
+
+    Private Sub cmdrestart_Click()
+        PictureBox4.Visible = False
+
+        'reset 
+        Dim i = inti
+        Dim j = intj
+
+        For i = 0 To 15
+            For j = 0 To 15
+                table(inti, intj) = 0
+            Next
+        Next
+
+
+        blackturn = True 'black first
+        boolstatus = True 'game running flag 
+        Label1.Text = "Black First"
+
+        'draw the chessboard
+        Call Me_Draw_Paint()
+
+    End Sub
+
+    Private Sub cmdrestart_Click(sender As Object, e As EventArgs) Handles cmdrestart.Click
+        PictureBox4.Visible = False
+
+        'reset 
+        Dim i = inti
+        Dim j = intj
+
+        For i = 0 To 15
+            For j = 0 To 15
+                table(inti, intj) = 0
+            Next
+        Next
+
+
+        blackturn = True 'black first
+        boolstatus = True 'game running flag 
+        Label1.Text = "Black First"
+        'draw the chessboard
+        Call Me_Draw_Paint()
+
+    End Sub
+
+    Private Sub Me_Draw_VoiceDown(vX As Integer, vY As Integer)
+        PictureBox4.Visible = False 'member pic 
+        'judge if the game is running
+        If boolstatus = False Then
+            Label1.Text = "End"
+            Exit Sub
+        End If
+
+
+
+        txtBox.Text = vX & "+" & vY & Chr(10)
+        'convert to pixel X/Y axis
+        intx = vX * 20 + 10
+        inty = vY * 20 + 10
+        'transfer coordinate into array "table"
+        inti = (intx - 10) / 20
+        intj = (inty - 10) / 20
+
+        'if there is alreay a chess piece exist
+        If table(inti, intj) <> 0 Then
+            Exit Sub
+        End If
+
+        'Draw chess pieces
+        If blackturn = True Then
+            'black
+
+            Me_Draw_PaintCircle(1, intx, inty)
+            table(inti, intj) = 1 'black=1 in array
+            Label1.Text = "White Turn"
+            PictureBox1.Image = Image.FromFile("C:\Users\JunY\Documents\Visual Studio 2012\Projects\SpeechChess2\chaoran.png")
+            PictureBox5.Image = Image.FromFile("C:\Users\JunY\Documents\Visual Studio 2012\Projects\SpeechChess2\nan_chess_white.png")
+
+        Else
+            'white
+            Me_Draw_PaintCircle(2, intx, inty)
+            table(inti, intj) = 2 'white=2 in array
+            Label1.Text = "Black Turn"
+            PictureBox1.Image = Image.FromFile("C:\Users\JunY\Documents\Visual Studio 2012\Projects\SpeechChess2\jun_chess.png")
+            PictureBox5.Image = Image.FromFile("C:\Users\JunY\Documents\Visual Studio 2012\Projects\SpeechChess2\nan_chess_black.png")
+        End If
+
+
+        'Circle (intx, inty), 8 
+
+        'judge whether anyone win
+        Call judgeman()
+
+        'turn 
+        blackturn = Not blackturn
+
+        If blackturn = True Then
+            RecoContext.Voice.Speak("it's white's turn now, next is black's turn!")
+        Else
+            RecoContext.Voice.Speak("it's black's turn now ,next is white's turn!")
+        End If
+    End Sub
+
+    Private Sub Me_Draw_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me_Draw.MouseDown
+
+        'judge if the game is running
+        If boolstatus = False Then
+            Label1.Text = "End"
+            Exit Sub
+        End If
+
+
+        If e.X < 10 Or e.X > 310 Or e.Y < 10 Or e.Y > 310 Then
+            Exit Sub
+        End If
+        'coordinate 
+        If (e.X - 10) Mod 20 < 10 Then
+            intx = e.X - (e.X - 10) Mod 20
+        Else
+            intx = e.X + 20 - (e.X - 10) Mod 20
+        End If
+        If (e.Y - 10) Mod 20 < 10 Then
+            inty = e.Y - (e.Y - 10) Mod 20
+        Else
+            inty = e.Y + 20 - (e.Y - 10) Mod 20
+        End If
+        txtBox.Text = intx & "+" & inty
+        'transfer coordinate into array "table"
+        inti = (intx - 10) / 20
+        intj = (inty - 10) / 20
+
+        'if there is alreay a chess piece exist
+        If table(inti, intj) <> 0 Then
+            Exit Sub
+        End If
+
+        'Draw chess pieces
+        If blackturn = True Then
+            'black
+
+            Me_Draw_PaintCircle(1, intx, inty)
+            table(inti, intj) = 1 'black=1 in array
+            Label1.Text = "White Turn"
+            PictureBox1.Image = Image.FromFile("C:\Users\JunY\Documents\Visual Studio 2012\Projects\SpeechChess2\chaoran.png")
+            PictureBox5.Image = Image.FromFile("C:\Users\JunY\Documents\Visual Studio 2012\Projects\SpeechChess2\nan_chess_white.png")
+        Else
+            'white
+            Me_Draw_PaintCircle(2, intx, inty)
+            table(inti, intj) = 2 'white=2 in array
+            Label1.Text = "Black Turn"
+            PictureBox1.Image = Image.FromFile("C:\Users\JunY\Documents\Visual Studio 2012\Projects\SpeechChess2\jun_chess.png")
+            PictureBox5.Image = Image.FromFile("C:\Users\JunY\Documents\Visual Studio 2012\Projects\SpeechChess2\nan_chess_black.png")
+        End If
+
+
+        'Circle (intx, inty), 8 
+
+        'judge whether anyone win
+        Call judgeman()
+
+        'turn 
+        blackturn = Not blackturn
+    End Sub
+
+
+    Private Sub Cmd_Close_Click(sender As Object, e As EventArgs) Handles Cmd_Close.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub cmdclose_Click(sender As Object, e As EventArgs) Handles cmdclose.Click
+        Application.Restart()
+    End Sub
+
+
+
+
+    Private Sub PictureBox5_Click(sender As Object, e As EventArgs) Handles PictureBox5.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
+
